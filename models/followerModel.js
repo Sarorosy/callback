4990376da27fc2e72bec3e0a304120b340ed2ed2
sarorosy <sn11std@gmail.com>
2025-10-01 17:27:48 +0530

    follower name

--- models/followerModel.js
+++ models/followerModel.js
@@ -27,53 +27,53 @@ const getAllActiveFollowers = (callback) => {
 
 // Get all followers
 const getAllFollowers = (usertype, userid, callback) => {
+  let sql = `
+    SELECT 
+      tbl_follower.id as followerid, 
+      tbl_follower.bookingid,
+      tbl_follower.follower_consultant_id,
+      tbl_follower.consultantid,
+      tbl_follower.addedon, 
+      tbl_follower.status as followstatus, 
+      tbl_booking.*, 
+      addedby.fld_name as admin_name, 
+      addedby.fld_email as admin_email, 
+      consultant.fld_name as consultant_name, 
+      follower_consultant.fld_name as follower_consultant_name,
+      tbl_user.fld_user_code as user_code, 
+      tbl_user.fld_name as user_name, 
+      tbl_user.fld_email as user_email, 
+      tbl_user.fld_decrypt_password as user_pass, 
+      tbl_user.fld_country_code as user_country_code, 
+      tbl_user.fld_phone as user_phone, 
+      tbl_user.fld_address, 
+      tbl_user.fld_city, 
+      tbl_user.fld_pincode, 
+      tbl_user.fld_country 
+    FROM tbl_follower 
+    JOIN tbl_booking ON tbl_booking.id = tbl_follower.bookingid 
+    LEFT JOIN tbl_admin as addedby ON tbl_booking.fld_addedby = addedby.id 
+    LEFT JOIN tbl_admin as consultant ON tbl_booking.fld_consultantid = consultant.id 
+    LEFT JOIN tbl_admin as follower_consultant ON tbl_follower.follower_consultant_id = follower_consultant.id
+    JOIN tbl_user ON tbl_booking.fld_userid = tbl_user.id
+  `;
 
+  const params = [];
 
-    let sql = `
-      SELECT 
-        tbl_follower.id as followerid, 
-        tbl_follower.bookingid,
-        tbl_follower.follower_consultant_id,
-        tbl_follower.consultantid,
-        tbl_follower.addedon, 
-        tbl_follower.status as followstatus, 
-        tbl_booking.*, 
-        addedby.fld_name as admin_name, 
-        addedby.fld_email as admin_email, 
-        consultant.fld_name as consultant_name, 
-        tbl_user.fld_user_code as user_code, 
-        tbl_user.fld_name as user_name, 
-        tbl_user.fld_email as user_email, 
-        tbl_user.fld_decrypt_password as user_pass, 
-        tbl_user.fld_country_code as user_country_code, 
-        tbl_user.fld_phone as user_phone, 
-        tbl_user.fld_address, 
-        tbl_user.fld_city, 
-        tbl_user.fld_pincode, 
-        tbl_user.fld_country 
-      FROM tbl_follower 
-      JOIN tbl_booking ON tbl_booking.id = tbl_follower.bookingid 
-      LEFT JOIN tbl_admin as addedby ON tbl_booking.fld_addedby = addedby.id 
-      LEFT JOIN tbl_admin as consultant ON tbl_booking.fld_consultantid = consultant.id 
-      JOIN tbl_user ON tbl_booking.fld_userid = tbl_user.id
-    `;
+  // If consultant, filter by their id
+  if (usertype === "CONSULTANT") {
+    sql += " WHERE tbl_follower.consultantid = ? OR tbl_follower.follower_consultant_id = ?";
+    params.push(userid, userid);
+  }
 
-    const params = [];
+  sql += " ORDER BY tbl_follower.id DESC";
 
-    // If consultant, filter by their id
-    if (usertype === "CONSULTANT") {
-      sql += " WHERE tbl_follower.consultantid = ? OR tbl_follower.follower_consultant_id = ?";
-      params.push(userid, userid);
-    }
-
-    sql += " ORDER BY tbl_follower.id DESC";
-
-    db.query(sql, params, (queryErr, results) => {
-      if (queryErr) return callback(queryErr, null);
-      return callback(null, results);
-    });
-  
+  db.query(sql, params, (queryErr, results) => {
+    if (queryErr) return callback(queryErr, null);
+    return callback(null, results);
+  });
 };
+
 
 const insertBookingHistory = (data, callback) => {
   const fields = Object.keys(data);
